beee9fcca97a5c92af00327f1e521dad87726588
otto-camp <ismailyarar4@gmail.com>
2023-10-23 18:14:17 +0300

    Remove fetch request from home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import { env } from "@/env.mjs";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "The Analytics",
 };
 
-export const dynamic = "force-dynamic";
-
 export default async function Home() {
-  const data = await fetch(`${env.NEXT_PUBLIC_APP_URL}/api/ping`).then(
-    async (res) => await res.json()
-  );
-  console.log(data);
-
   return (
     <main className="p-4 grid gap-4">
       <h1>Hello World</h1>
